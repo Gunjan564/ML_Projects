76e4dd9925938d093cd9b5c3fc95f2ee8a7610de
Gunjan <>
2025-03-23 23:41:58 +0530

    commit

--- Movie Recommender/download_data.py
+++ Movie Recommender/download_data.py
@@ -6,8 +6,9 @@ file_links = {
     "new_movies.zip": "https://drive.google.com/file/d/1Wcqf_EGRrIOQVj10xIeTxceVrAzeccrR/view?usp=drive_link"
 }
 
-filename = "similarity.pkl"
-gdown.download(f"https://drive.google.com/uc?id=1Umphlynk-I4_AlJJdgMRebd2clquL0PB", filename, quiet=False)
+file_1 = "similarity.pkl"
+file_2 = "new_movies.pkl"
+gdown.download(f"https://drive.google.com/uc?id=1Umphlynk-I4_AlJJdgMRebd2clquL0PB", file_1, quiet=False)
 print("similarity.pkl Downloaded!!")
-gdown.download(f"https://drive.google.com/uc?id=1Umphlynk-I4_AlJJdgMRebd2clquL0PB", filename, quiet=False)
-print("similarity.pkl Downloaded!!")
+gdown.download(f"https://drive.google.com/uc?id=1Wcqf_EGRrIOQVj10xIeTxceVrAzeccrR", file_2, quiet=False)
+print("new_movies.pkl Dowloaded!!")
