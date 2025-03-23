9f9714921c9285b7c7e166633bc1b6cbd4639a95
Gunjan <>
2025-03-24 00:22:20 +0530

    commit

--- Movie Recommender/MovieRecommender.py
+++ Movie Recommender/MovieRecommender.py
@@ -37,22 +37,6 @@ def recommend(movie):
 
   return recommended_movie_names,recommended_movie_posters
 
-# ------------dowloading pkl files---------------
-
-file_links = {
-    "similarity.pkl": "https://drive.google.com/file/d/1Umphlynk-I4_AlJJdgMRebd2clquL0PB/view?usp=drive_link",
-    "new_movies.pkl": "https://drive.google.com/file/d/1Wcqf_EGRrIOQVj10xIeTxceVrAzeccrR/view?usp=drive_link"
-}
-
-file_1 = "similarity.pkl"
-file_2 = "new_movies.pkl"
-gdown.download(f"https://drive.google.com/uc?id=1Umphlynk-I4_AlJJdgMRebd2clquL0PB", file_1, quiet=False)
-print("similarity.pkl Downloaded!!")
-gdown.download(f"https://drive.google.com/uc?id=1Wcqf_EGRrIOQVj10xIeTxceVrAzeccrR", file_2, quiet=False)
-print("new_movies.pkl Dowloaded!!")
-# -----------------------------------------------
-
-
 # Load the processed data
 movies_dict = pickle.load(open(r'new_movies.pkl', 'rb'))
 similarity = pickle.load(open(r'similarity.pkl','rb'))
